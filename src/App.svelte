27bb39e3d1ac63abbf7cdd9d015c3551a8f87e8e
Don Gumaygay <dngumayagay@gmail.com>
2022-08-15 23:23:19 +0800

    init

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+    let globalObject = {};
+    let nameFn = "MyNewFunction";
+
+    const createFn = function (object, functionName, Body) {
+        object[functionName] = new Function(Body);
+    };
+
+    createFn(globalObject, nameFn, "return (arguments[0] + arguments[1])/2");
+    createFn(globalObject, "sum", "return (arguments[0]+arguments[1])");
+    console.log(globalObject[nameFn](10, 20));
+    console.log(globalObject["sum"](10, 20));
+</script>
